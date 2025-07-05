885ce66cc35a10bee0e3ed56163d56bbe425a897
rivst <me@riv.st>
2025-07-05 19:20:39 +0300

    fix: Vercel config

--- api/index.py
+++ api/index.py
@@ -2,19 +2,17 @@
 Vercel entry point for the Flask app
 """
 
+import os
 import sys
 from pathlib import Path
 from penne import create_app
 
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
+os.environ.setdefault("FLASK_CONFIG", "prod")
+
 
 app = create_app()
-
-
-def handler(event, context):
-    return app(event, context)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
